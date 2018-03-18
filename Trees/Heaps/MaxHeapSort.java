ca967b95902b7d4dfd5e8cd46ce0205f48304491
Amogh Mishra <amogh@kenome.io>
2018-03-18 16:57:47 +0530

    Update Max Heapsort

--- Trees/Heaps/MaxHeapSort.java
+++ Trees/Heaps/MaxHeapSort.java
@@ -8,17 +8,7 @@ public class MaxHeapSort
 		heap.buildHeap(arr, arr.length);
 		heap.heapSort(arr);
 		System.out.println("Sorted array is : "+ Arrays.toString(arr) );
-		printArray(arr);
 	}
-
- /* A utility function to print array of size n */
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
-        System.out.println();
-    }
 
 	private void heapSort(int heap[])
 	{
